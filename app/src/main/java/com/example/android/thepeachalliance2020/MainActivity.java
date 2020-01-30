515fbde1ab447c214906b42881ca3391e83b65ae
Morris, Andrew Groth <amorris64@gatech.edu>
2020-01-29 22:36:05 -0500

    Added some var's to InputManager and implemented version text on home page.

--- app/src/main/java/com/example/android/thepeachalliance2020/MainActivity.java
+++ app/src/main/java/com/example/android/thepeachalliance2020/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.android.thepeachalliance2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.android.thepeachalliance2020.Managers.InputManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static TextView version_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        initViews();
+
+        // InputManager.recoverUserData(); //Add this in its where it gets the last used data.
+        updateUserData();
     }
+
+    //Set all UI text values
+    public static void updateUserData() {
+        version_number.setText(String.valueOf("Version: " + InputManager.mAppVersion));
+    }
+
+
+    public void initViews() {
+        version_number = findViewById(R.id.version_number);
+    }
+
 }
--- app/src/main/java/com/example/android/thepeachalliance2020/Managers/InputManager.java
+++ app/src/main/java/com/example/android/thepeachalliance2020/Managers/InputManager.java
@@ -1,4 +1,29 @@
 package com.example.android.thepeachalliance2020.Managers;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class InputManager {
+    //Match Data Holders
+    //Below holds match data
+    public static JSONArray mRealTimeMatchData = new JSONArray();
+    //keys that only appear once in data
+    public static JSONObject mOneTimeMatchData;
+    //Below is finaldata to be inputted to QR
+    public static JSONObject mRealTimeInputtedData;
+
+    //Main Inputs
+    public static String matchKey = "1746Q1-1";
+    public static String mAllianceColor = "";
+    public static String mScoutName = "unselected";
+    public static String mTabletType = "unselected";
+
+    public static int mScoutId = 0;
+    public static int mMatchNum = 0;
+    public static int mTeamNum = 0;
+    public static int mCycleNum = 0;
+
+    public static String mAppVersion = "1.0";
+
 }
